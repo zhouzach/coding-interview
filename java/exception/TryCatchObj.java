5d694a95892e97f7ee076126be6bc98d726109d7
Zhou Zach <wander669@gmail.com>
2019-06-08 23:32:46 +0800

    add exception

---
+++ java/exception/TryCatchObj.java
@@ -0,0 +1,25 @@
+package exception;
+
+public class TryCatchObj {
+    public static void main(String[] args) {
+        int n = getValueFromFinally(0);
+        System.out.println("n is " + n);
+
+    }
+
+    public static int getValueFromFinally(int n) {
+        int ret;
+        try {
+            ret = 2 / n;
+            return ret;
+        } catch (Exception e) {
+            ret = 10;
+            return ret;
+        } finally {
+            ret = 100;
+
+            return ret;
+        }
+
+    }
+}
